95509503e743e87550fb9e71b950b9551451bc1f
Kate Kasperovich <144998054+kaskate00@users.noreply.github.com>
2023-12-22 12:30:46 +0300

    Delete Dev directory

--- Dev/weather/app/app.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import weatherRouter from './routes/weatherRoutes.js'
-
-const app = express()
-
-app.use(express.json())
-app.use('/weather', weatherRouter)
-
-export default app
--- Dev/weather/app/routes/weatherRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import weatherController from '../controllers/weatherController.js'
-
-const router = express.Router()
-
-router.get('/:city/:date/', weatherController.getWeather)
-
-export default router
